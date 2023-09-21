f86060c3cab3c3a8939ce4900aaf15848e29c138
bryanken00 <bryanken01230@gmail.com>
2023-09-22 00:32:04 +0800

    Prototype UPLOAD IMG

--- KBNThesis/src/testingForecasting/FileUploader.java
+++ KBNThesis/src/testingForecasting/FileUploader.java
@@ -1,31 +1,24 @@
 package testingForecasting;
+
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JProgressBar;
 import javax.swing.filechooser.FileNameExtensionFilter;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.EntityBuilder;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
 
 public class FileUploader {
     private JFrame frame;
-    private File selectedFile;
+    private JPanel panel;
 
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
@@ -50,62 +43,74 @@ public class FileUploader {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().setLayout(null);
 
-        JButton btnBrowse = new JButton("Browse");
-        btnBrowse.setBounds(150, 30, 150, 30);
-        frame.getContentPane().add(btnBrowse);
+        panel = new JPanel();
+        panel.setBounds(10, 10, 414, 142);
+        frame.getContentPane().add(panel);
+        panel.setLayout(null);
 
-        btnBrowse.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                JFileChooser fileChooser = new JFileChooser();
-                FileNameExtensionFilter filter = new FileNameExtensionFilter("Images", "jpg", "jpeg", "png", "gif");
-                fileChooser.setFileFilter(filter);
-                int returnValue = fileChooser.showOpenDialog(null);
-                if (returnValue == JFileChooser.APPROVE_OPTION) {
-                    selectedFile = fileChooser.getSelectedFile();
-                }
-            }
-        });
-
-        JButton btnUpload = new JButton("Upload");
-        btnUpload.setBounds(150, 70, 150, 30);
-        frame.getContentPane().add(btnUpload);
+        JButton btnUpload = new JButton("Upload File");
+        btnUpload.setBounds(10, 10, 150, 30);
+        panel.add(btnUpload);
 
         btnUpload.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                if (selectedFile != null) {
+                JFileChooser fileChooser = new JFileChooser();
+                
+                // Create a filter to only accept image files with extensions jpg, jpeg, png, and gif
+                FileNameExtensionFilter imageFilter = new FileNameExtensionFilter(
+                    "Image Files", "jpg", "jpeg", "png", "gif");
+                
+                fileChooser.setFileFilter(imageFilter); // Set the filter on the file chooser
+                
+                int returnValue = fileChooser.showOpenDialog(null);
+                
+                if (returnValue == JFileChooser.APPROVE_OPTION) {
+                    File selectedFile = fileChooser.getSelectedFile();
                     uploadFile(selectedFile);
-                } else {
-                    JOptionPane.showMessageDialog(null, "Please select a file to upload.");
                 }
             }
         });
+        
     }
 
+
     private void uploadFile(File file) {
+        String targetURL = "http://localhost/webdevelopment/thesis1_website/Products/upload.php";
+
         try {
-            HttpClient httpclient = HttpClients.createDefault();
-            HttpPost httppost = new HttpPost("http://localhost/webdevelopment/thesis1_website/Products/upload.php");
+            URL url = new URL(targetURL);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestMethod("POST");
 
-            // Prepare the file to be uploaded
-            EntityBuilder entityBuilder = EntityBuilder.create();
-            entityBuilder.setFile(file);
-            HttpEntity entity = entityBuilder.build();
+            String boundary = Long.toHexString(System.currentTimeMillis());
+            connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
 
-            // Set up the request parameters
-            List<NameValuePair> params = new ArrayList<NameValuePair>();
-            params.add(new BasicNameValuePair("uploadfile", file.getName()));
-            httppost.setEntity(entity);
+            try (OutputStream os = connection.getOutputStream()) {
+                os.write(("--" + boundary + "\r\n").getBytes());
+                os.write(("Content-Disposition: form-data; name=\"file\"; filename=\"" + file.getName() + "\"\r\n").getBytes());
+                os.write(("Content-Type: application/octet-stream\r\n\r\n").getBytes());
 
-            // Execute the request
-            HttpResponse response = httpclient.execute(httppost);
-
-            // Handle the response (you can add more error handling here)
-            int statusCode = response.getStatusLine().getStatusCode();
-            if (statusCode == 200) {
-                JOptionPane.showMessageDialog(null, "File uploaded successfully!");
-            } else {
-                JOptionPane.showMessageDialog(null, "File upload failed. Status Code: " + statusCode);
+                byte[] fileBytes = java.nio.file.Files.readAllBytes(file.toPath());
+                os.write(fileBytes);
+                os.write(("\r\n--" + boundary + "--\r\n").getBytes());
             }
+
+            int responseCode = connection.getResponseCode();
+            System.out.println("Response Code: " + responseCode);
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                // Read and display the response from the PHP script
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        System.out.println("Response: " + line);
+                    }
+                }
+            } else {
+                System.out.println("Upload failed.");
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
