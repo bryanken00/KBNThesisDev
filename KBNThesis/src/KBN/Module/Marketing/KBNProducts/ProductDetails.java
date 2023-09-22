72b42ed9797e1f947ae084632db630224add1495
bryanken00 <bryanken01230@gmail.com>
2023-09-23 00:22:35 +0800

    Marketing - Products(Implementing Upload Img)

--- KBNThesis/src/KBN/Module/Marketing/KBNProducts/ProductDetails.java
+++ KBNThesis/src/KBN/Module/Marketing/KBNProducts/ProductDetails.java
@@ -5,7 +5,9 @@ import java.awt.FlowLayout;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JFileChooser;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
 
 import KBN.commons.DbConnection;
@@ -16,27 +18,39 @@ import javax.swing.JOptionPane;
 import java.awt.Font;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import java.awt.Color;
 import java.awt.Dimension;
 
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.ImageIcon;
 import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.JSeparator;
+import javax.swing.JTextPane;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.awt.event.ActionEvent;
 
-public class ProductDetails extends JDialog {
+public class ProductDetails extends JDialog implements ActionListener{
 	
 	public JTextField txtProdName;
 	public JTextField txtPrice;
 	public JTextField txtVariant;
-	public JTextField txtDescription;
-	public JTextField txtIngredients;
-	public JTextField txtManual;
 	public JLabel lblImg;
 	
 	private DbConnection dbConn;
@@ -44,20 +58,35 @@ public class ProductDetails extends JDialog {
 	private ResultSet rs;
 	
 	private String prodID = "";
-	private JTextField txtCategory;
+	private JComboBox cbCategory;
 	
 	public void ProductDetails(String prodID) {
 		this.prodID = prodID;
 		DataSetters();
 	}
+	
+	private ArrayList<String> arrCb;
+	private ArrayList<String> arrCBIdentifier;
+	private JTextPane txtDescription;
+	private JTextPane txtIngredients;
+	private JButton btnSave;
+	private JTextPane txtManual;
+	private JButton btnAddImg;
 
 	public ProductDetails() {
 		setResizable(false);
-		setBounds(100, 100, 842, 554);
+		setBounds(100, 100, 989, 522);
 		setModal(true);
 		getContentPane().setLayout(null);
 		
 		dbConn = new DbConnection();
+		try {
+			st = dbConn.getConnection().createStatement();
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Error: StatementProdDetails: " + e.getMessage());
+		}
+		arrCb  = new ArrayList<>();
+		arrCBIdentifier  = new ArrayList<>();
 		
 		//set UI to center of screen
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
@@ -67,93 +96,157 @@ public class ProductDetails extends JDialog {
         int y = (dim.height-h)/2;
         this.setLocation(x, y);
 		
+		JPanel imgPanel = new JPanel();
+		imgPanel.setBackground(new Color(255, 255, 255));
+		imgPanel.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
+		imgPanel.setBounds(10, 11, 310, 461);
+		getContentPane().add(imgPanel);
+		imgPanel.setLayout(null);
+		
 		lblImg = new JLabel("");
+		lblImg.setBackground(Color.LIGHT_GRAY);
+		lblImg.setBounds(10, 11, 290, 353);
+		imgPanel.add(lblImg);
 		lblImg.setHorizontalAlignment(SwingConstants.CENTER);
-		lblImg.setBounds(10, 11, 310, 493);
-		getContentPane().add(lblImg);
+		
+		btnAddImg = new JButton("Upload Product Img");
+		btnAddImg.setForeground(Color.WHITE);
+		btnAddImg.setFont(new Font("MS Reference Sans Serif", Font.BOLD, 18));
+		btnAddImg.setFocusable(false);
+		btnAddImg.setBorderPainted(false);
+		btnAddImg.setBackground(new Color(8, 104, 0));
+		btnAddImg.setBounds(34, 411, 241, 35);
+		imgPanel.add(btnAddImg);
+		
+		JPanel dataPanel = new JPanel();
+		dataPanel.setBackground(new Color(255, 255, 255));
+		dataPanel.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
+		dataPanel.setBounds(330, 11, 633, 461);
+		getContentPane().add(dataPanel);
+		dataPanel.setLayout(null);
 		
 		JLabel lblNewLabel_1 = new JLabel("Product:");
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblNewLabel_1.setBounds(330, 23, 140, 28);
-		getContentPane().add(lblNewLabel_1);
+		lblNewLabel_1.setBounds(10, 84, 140, 28);
+		dataPanel.add(lblNewLabel_1);
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		
 		txtProdName = new JTextField();
+		txtProdName.setBounds(10, 114, 286, 28);
+		dataPanel.add(txtProdName);
 		txtProdName.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		txtProdName.setBounds(518, 23, 286, 28);
-		getContentPane().add(txtProdName);
 		txtProdName.setColumns(10);
 		
 		JLabel lblNewLabel_1_1 = new JLabel("Price:");
-		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblNewLabel_1_1.setBounds(330, 83, 140, 28);
-		getContentPane().add(lblNewLabel_1_1);
+		lblNewLabel_1_1.setBounds(250, 167, 140, 28);
+		dataPanel.add(lblNewLabel_1_1);
+		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		
 		txtPrice = new JTextField();
+		txtPrice.setBounds(250, 197, 286, 28);
+		dataPanel.add(txtPrice);
 		txtPrice.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		txtPrice.setColumns(10);
-		txtPrice.setBounds(518, 83, 286, 28);
-		getContentPane().add(txtPrice);
 		
 		JLabel lblNewLabel_1_1_1 = new JLabel("Variant:");
-		lblNewLabel_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblNewLabel_1_1_1.setBounds(330, 143, 140, 28);
-		getContentPane().add(lblNewLabel_1_1_1);
+		lblNewLabel_1_1_1.setBounds(10, 167, 140, 28);
+		dataPanel.add(lblNewLabel_1_1_1);
+		lblNewLabel_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		
 		txtVariant = new JTextField();
+		txtVariant.setBounds(10, 197, 220, 28);
+		dataPanel.add(txtVariant);
 		txtVariant.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		txtVariant.setColumns(10);
-		txtVariant.setBounds(518, 143, 286, 28);
-		getContentPane().add(txtVariant);
 		
 		JLabel lblNewLabel_1_1_1_1 = new JLabel("Category");
-		lblNewLabel_1_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblNewLabel_1_1_1_1.setBounds(330, 203, 140, 28);
-		getContentPane().add(lblNewLabel_1_1_1_1);
+		lblNewLabel_1_1_1_1.setBounds(327, 84, 140, 28);
+		dataPanel.add(lblNewLabel_1_1_1_1);
+		lblNewLabel_1_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		
 		JLabel lblNewLabel_1_1_1_1_1 = new JLabel("Description:");
-		lblNewLabel_1_1_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblNewLabel_1_1_1_1_1.setBounds(330, 263, 140, 28);
-		getContentPane().add(lblNewLabel_1_1_1_1_1);
-		
-		txtDescription = new JTextField();
-		txtDescription.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		txtDescription.setBounds(518, 263, 286, 28);
-		getContentPane().add(txtDescription);
-		txtDescription.setColumns(10);
+		lblNewLabel_1_1_1_1_1.setBounds(10, 250, 140, 28);
+		dataPanel.add(lblNewLabel_1_1_1_1_1);
+		lblNewLabel_1_1_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		
 		JLabel lblNewLabel_1_1_1_1_1_1 = new JLabel("Ingredients:");
-		lblNewLabel_1_1_1_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblNewLabel_1_1_1_1_1_1.setBounds(330, 353, 140, 28);
-		getContentPane().add(lblNewLabel_1_1_1_1_1_1);
-		
-		txtIngredients = new JTextField();
-		txtIngredients.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		txtIngredients.setColumns(10);
-		txtIngredients.setBounds(518, 353, 286, 28);
-		getContentPane().add(txtIngredients);
+		lblNewLabel_1_1_1_1_1_1.setBounds(327, 250, 140, 28);
+		dataPanel.add(lblNewLabel_1_1_1_1_1_1);
+		lblNewLabel_1_1_1_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		
 		JLabel lblNewLabel_1_1_1_1_1_1_1 = new JLabel("Manual:");
-		lblNewLabel_1_1_1_1_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblNewLabel_1_1_1_1_1_1_1.setBounds(330, 443, 140, 28);
-		getContentPane().add(lblNewLabel_1_1_1_1_1_1_1);
+		lblNewLabel_1_1_1_1_1_1_1.setBounds(10, 369, 140, 28);
+		dataPanel.add(lblNewLabel_1_1_1_1_1_1_1);
+		lblNewLabel_1_1_1_1_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		
-		txtManual = new JTextField();
-		txtManual.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		txtManual.setColumns(10);
-		txtManual.setBounds(518, 443, 286, 28);
-		getContentPane().add(txtManual);
+		cbCategory = new JComboBox();
+		cbCategory.setBounds(327, 114, 286, 28);
+		dataPanel.add(cbCategory);
 		
-		txtCategory = new JTextField();
-		txtCategory.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		txtCategory.setColumns(10);
-		txtCategory.setBounds(518, 203, 286, 28);
-		getContentPane().add(txtCategory);
+		JLabel lblTitle = new JLabel("Add Product");
+		lblTitle.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		lblTitle.setBounds(10, 11, 170, 39);
+		dataPanel.add(lblTitle);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBackground(Color.BLACK);
+		separator.setBounds(10, 53, 613, 19);
+		dataPanel.add(separator);
+		
+		btnSave = new JButton("Save");
+		btnSave.setForeground(Color.WHITE);
+		btnSave.setFont(new Font("MS Reference Sans Serif", Font.BOLD, 18));
+		btnSave.setFocusable(false);
+		btnSave.setBorderPainted(false);
+		btnSave.setBackground(new Color(8, 104, 0));
+		btnSave.setBounds(511, 11, 112, 35);
+		dataPanel.add(btnSave);
+		
+		txtDescription = new JTextPane();
+		txtDescription.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
+		JScrollPane descriptionScrollPane = new JScrollPane(txtDescription);
+		descriptionScrollPane.setBounds(10, 280, 286, 78);
+		dataPanel.add(descriptionScrollPane);
+
+		txtIngredients = new JTextPane();
+		txtIngredients.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
+		JScrollPane ingredientsScrollPane = new JScrollPane(txtIngredients);
+		ingredientsScrollPane.setBounds(327, 280, 286, 78);
+		dataPanel.add(ingredientsScrollPane);
+		
+		txtManual = new JTextPane();
+		txtManual.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
+		txtManual.setBounds(10, 399, 286, 51);
+		dataPanel.add(txtManual);
+		
+		btnAddImg.addActionListener(this);
+		btnSave.addActionListener(this);
 	}
 	
+	public void cbSetter() {
+		try {
+			String cbSQL = "SELECT * FROM tblproductcategories";
+			st.execute(cbSQL);
+			rs = st.getResultSet();
+			arrCb.clear();
+			arrCBIdentifier.clear();
+			while(rs.next()) {
+				arrCBIdentifier.add(rs.getString(1));
+				arrCb.add(rs.getString(2));
+			}
+	        DefaultComboBoxModel<String> model = (DefaultComboBoxModel<String>) cbCategory.getModel();
+	        model.removeAllElements();
+	        
+	        for (String item : arrCb) {
+	            model.addElement(item);
+	        }
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Error cbSetter: " + e.getMessage());
+		}
+	}
+
 	private void DataSetters() {
 		try {
-			st = dbConn.getConnection().createStatement();
-			
+			cbSetter();
 			String SQL = "SELECT prodImg, prodName, prodVolume, prodPrice, prodCategory, Description, Ingredients, Howtouse FROM tblproducts WHERE prodID = '" + prodID + "'";
 			st.execute(SQL);
 			
@@ -165,7 +258,11 @@ public class ProductDetails extends JDialog {
 				txtProdName.setText(rs.getString(2));
 				txtVariant.setText(rs.getString(3));
 				txtPrice.setText(rs.getString(4));
-				txtCategory.setText(rs.getString(5));
+//				cbCategory.addItem(rs.getString(5));
+				for(int i = 0; i < arrCBIdentifier.size(); i++) {
+					if(arrCBIdentifier.get(i).equals(rs.getString(5)))
+						cbCategory.setSelectedIndex(i);
+				}
 				txtDescription.setText(rs.getString(6));
 				txtIngredients.setText(rs.getString(7));
 				txtManual.setText(rs.getString(8));
@@ -195,5 +292,70 @@ public class ProductDetails extends JDialog {
 		
 		return scaledImage;
 	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == btnSave) {
+			
+		}
+		if(e.getSource() == btnAddImg) {
+            JFileChooser fileChooser = new JFileChooser();
+            
+            // Create a filter to only accept image files with extensions jpg, jpeg, png, and gif
+            FileNameExtensionFilter imageFilter = new FileNameExtensionFilter(
+                "Image Files", "jpg", "jpeg", "png", "gif");
+            
+            fileChooser.setFileFilter(imageFilter); // Set the filter on the file chooser
+            
+            int returnValue = fileChooser.showOpenDialog(null);
+            
+            if (returnValue == JFileChooser.APPROVE_OPTION) {
+                File selectedFile = fileChooser.getSelectedFile();
+                uploadFile(selectedFile);
+            }
+		}
+	}
 	
+	private void uploadFile(File file) {
+        String targetURL = "http://localhost/webdevelopment/thesis1_website/Products/upload.php";
+
+        try {
+            URL url = new URL(targetURL);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestMethod("POST");
+
+            String boundary = Long.toHexString(System.currentTimeMillis());
+            connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+
+            try (OutputStream os = connection.getOutputStream()) {
+                os.write(("--" + boundary + "\r\n").getBytes());
+                os.write(("Content-Disposition: form-data; name=\"file\"; filename=\"" + file.getName() + "\"\r\n").getBytes());
+                os.write(("Content-Type: application/octet-stream\r\n\r\n").getBytes());
+
+                byte[] fileBytes = java.nio.file.Files.readAllBytes(file.toPath());
+                os.write(fileBytes);
+                os.write(("\r\n--" + boundary + "--\r\n").getBytes());
+            }
+
+            int responseCode = connection.getResponseCode();
+            System.out.println("Response Code: " + responseCode);
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                // Read and display the response from the PHP script
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        System.out.println("Response: " + line);
+                        lblImg.setIcon(new ImageIcon(imgLoader(line)));
+                    }
+                }
+            } else {
+                System.out.println("Upload failed.");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
